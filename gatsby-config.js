b3d104e79f8637614645721066cac62b09abd438
Golden Lion Tamarin <golden.tamarin.lion@gmail.com>
2021-04-12 18:39:49 -0300

    invicta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Invicta Serviços 24 horas',
+    author: 'Lucas Nable',
+    description: 'Invicta serviços 24 horas',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
